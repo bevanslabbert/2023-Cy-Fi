343e73699c86851446d9b3f46c0f65d76b3c705d
Bevan Slabbert <bevanslabbert@gmail.com>
2023-05-17 10:46:17 +0200

    updated observation space

--- PythonBot copy/RL/customEnvironment.py
+++ PythonBot copy/RL/customEnvironment.py
@@ -5,22 +5,24 @@ from gym.spaces import Discrete, Box
 from Game.action import Action
 from GameConnection import send
 
+
 class Environment(gym.Env):
+    def __init__(self, state):
+        self.observation_shape = (10, 16)
 
-    def __init__(self,state):
+        # Define the lower and upper bounds for the integer values
+        self.low = 0
+        self.high = 6
 
+        # Create a custom observation space
+        self.observation_space = spaces.Box(
+            low=self.low, high=self.high, shape=self.observation_shape, dtype=np.int32
+        )
         self.payload = state
 
         # Actions we can take
         self.action_space = Discrete(len(Action))
 
-        # Hero window
-        self.observation_space = spaces.Dict({
-            'x': spaces.Discrete(10000),
-            'y': spaces.Discrete(10000),
-            'level': spaces.Discrete(5)
-        })
-        
         # Start state
         self.state = state["heroWindow"]
         self.collected = state["collected"]
@@ -28,26 +30,24 @@ class Environment(gym.Env):
 
     def step(self, action, state, game_conn):
         send.send_action(game_conn, action.BotCommand(state.bot_id, action))
-        
+
         # Sleep until next tick updates hero state
-        while(self.state == state.bot_state["heroWindow"]):
+        while self.state == state.bot_state["heroWindow"]:
             time.sleep(0.5)
-        
+
         # If got a collectible, positive reward
-        if(state.bot_state["collected"] > self.payload["collected"]):
+        if state.bot_state["collected"] > self.payload["collected"]:
             reward += 1
 
         # If passed level, positive reward
-        if(state.bot_state["level"] > self.payload["level"]):
+        if state.bot_state["level"] > self.payload["level"]:
             reward += 100
 
         # If touching hazard, negative reward
 
-
         # Placeholder for info
         info = {}
         return state.bot_state["heroWindow"], reward, False, info
-
 
     def reset(self):
         # Reset connection to runner, and execute constructor code
